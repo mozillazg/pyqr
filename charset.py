c1a008e9f1f2647d0392aa4da30b841e5d6cd44c
Mozillazg <opensource.mozillazg@gmail.com>
2012-07-10 10:14:22 +0800

    Modified   charset.py

--- charset.py
+++ charset.py
@@ -6,7 +6,7 @@
 
 def utf8(stri):
     charests = ('utf8', 'gbk', 'gb2312', 'big5', 'ascii',
-                'shift_jis', 'euc_jp', 'euc_kr', 'latin1', 'latin2', 'latin9', 'latin10', 'koi8_r', 'cyrillic', 'utf16', 'utf32'
+                'shift_jis', 'euc_jp', 'euc_kr', 'iso2022_kr', 'latin1', 'latin2', 'latin9', 'latin10', 'koi8_r', 'cyrillic', 'utf16', 'utf32'
                  )
     str_ = stri
     for i in charests:
